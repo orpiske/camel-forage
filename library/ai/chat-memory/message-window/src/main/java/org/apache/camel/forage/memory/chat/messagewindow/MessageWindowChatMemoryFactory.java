0d8a47834e928a467973b3ab5be09d8248962f41
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-08-08 08:55:20 +0200

    There must be a single persistent store for the memory to be provided

--- library/ai/chat-memory/message-window/src/main/java/org/apache/camel/forage/memory/chat/messagewindow/MessageWindowChatMemoryFactory.java
+++ library/ai/chat-memory/message-window/src/main/java/org/apache/camel/forage/memory/chat/messagewindow/MessageWindowChatMemoryFactory.java
@@ -3,18 +3,30 @@ package org.apache.camel.forage.memory.chat.messagewindow;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import org.apache.camel.forage.core.ai.ChatMemoryFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MessageWindowChatMemoryFactory implements ChatMemoryFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(MessageWindowChatMemoryFactory.class);
 
+    private static final PersistentChatMemoryStore PERSISTENT_CHAT_MEMORY_STORE = new PersistentChatMemoryStore();
+    private final ChatMemoryProvider chatMemoryProvider;
+
+    public MessageWindowChatMemoryFactory() {
+        chatMemoryProvider = getChatMemoryProvider();
+    }
 
     @Override
-    public ChatMemoryProvider newChatMemory() {
-        System.out.println("Creating MessageWindowChatMemoryFactory");
-        ChatMemoryProvider chatMemoryProvider = memoryId -> MessageWindowChatMemory.builder()
+    public synchronized ChatMemoryProvider newChatMemory() {
+        return chatMemoryProvider;
+    }
+
+    private static ChatMemoryProvider getChatMemoryProvider() {
+        LOG.trace("Creating MessageWindowChatMemoryFactory");
+        return memoryId -> MessageWindowChatMemory.builder()
                 .id(memoryId)
                 .maxMessages(10)
-                .chatMemoryStore(new PersistentChatMemoryStore())
+                .chatMemoryStore(PERSISTENT_CHAT_MEMORY_STORE)
                 .build();
-        return chatMemoryProvider;
     }
 }
